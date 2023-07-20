50177096051b4b24071ed4c60bb5cc6381bcb207
Humberto Navarro <a320669@uach.mx>
2023-07-19 18:06:33 -0600

    Set up authentication with google and add mongodb connection script

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
         <body>
-            <div className='main'>
-                <div className="gradient"></div>
-            </div>
+            <Provider>
+                <div className='main'>
+                    <div className="gradient"></div>
+                </div>
 
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
