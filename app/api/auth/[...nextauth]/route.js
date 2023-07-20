2759e2c1bde00948a2e332f5b4c43d91ac3cf4a3
Humberto Navarro <a320669@uach.mx>
2023-07-20 16:08:53 -0600

    Create user model and add user verification to signIn function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
@@ -9,13 +12,33 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
     async signIn({ profile }) {
         try {
-            
-        } catch (error) {
+            await connectToDB();
 
+            //user exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
+            //user doesnt exist
+            if(!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
+            return true;
+        } catch (error) {
+            return false;
         }
     },
 })
