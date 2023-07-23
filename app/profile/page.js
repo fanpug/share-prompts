3d14c152d4f25508207a12ce87e3e2992ce845d4
Humberto Navarro <a320669@uach.mx>
2023-07-22 23:04:38 -0600

    Implement handle delete and edit functions add update-prompt page reusing Form component

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,12 +1,14 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react"
 
 import Profile from '@components/profile';
 
 const MyProfile = () => {
+    const router = useRouter();
+
     const { data: session } = useSession();
 
     const [posts, setPosts] = useState([])
@@ -24,12 +26,26 @@ const MyProfile = () => {
         }
     }, [])
 
-    const handleEdit = () => {
-
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
     }
 
-    const handleDelete = async () => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                });
+
+                const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+                setPosts(filteredPosts);
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }
 
 
