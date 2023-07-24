686033142cabc74069b83e44c98b36dfbc77d649
Humberto Navarro <a320669@uach.mx>
2023-07-24 15:32:49 -0600

    Implement viewing other users profile

--- app/profile/[id]/page.js
+++ app/profile/[id]/page.js
@@ -1,63 +1,34 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 
 import Profile from '@components/Profile';
 
-const PublicProfile = () => {
-    const router = useRouter();
+const PublicProfile = ({ params }) => {
     const searchParams = useSearchParams();
-    const promptId = searchParams.get("id");
-
-    const { data: session } = useSession();
+    const username = searchParams.get("username");
 
     const [posts, setPosts] = useState([])
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const response = await fetch(`/api/users/${params.id}/posts`);
             const data = await response.json();
 
-            setPosts(data)
+            setPosts(data);
         }
 
-        if (session?.user.id) {
+        if (params.id) {
             fetchPosts();
         }
     }, [])
 
-    const handleEdit = (post) => {
-        router.push(`/update-prompt?id=${post._id}`);
-    }
-
-    const handleDelete = async (post) => {
-        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
-
-        if (hasConfirmed) {
-            try {
-                await fetch(`/api/prompt/${post._id.toString()}`, {
-                    method: 'DELETE'
-                });
-
-                const filteredPosts = posts.filter((p) => p._id !== post._id);
-
-                setPosts(filteredPosts);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    }
-
-
     return (
         <Profile
-            name="My"
-            desc="Welcome to your profile page"
+            name={username}
+            desc={`Welcome to ${username}'s profile page`}
             data={posts}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
         />
     )
 }
